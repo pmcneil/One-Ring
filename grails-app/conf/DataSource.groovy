8da6e5c19c693392aab8311993df464e23a50aaf
Peter McNeil <pmcneil@mail.mcneils.net>
2011-11-14 20:44:49 +1100

    put back a basic hibernate config to make CXF work again :-/

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,4 +1,13 @@
 dataSource {
+    pooled = true
+    driverClassName = "org.hsqldb.jdbcDriver"
+    username = "sa"
+    password = ""
+}
+hibernate {
+    cache.use_second_level_cache = true
+    cache.use_query_cache = true
+    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
 }
 // environment specific settings
 environments {
